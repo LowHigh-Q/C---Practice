66b01c44c0b4ae6018d48adf40a6ff60dddaf0b8
HEMANTH R <120473382+LowHigh-Q@users.noreply.github.com>
2025-01-28 13:16:25 +0530

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int laastind = 0;
+        int[] aarr = new int[4];
+        Console.WriteLine("Enter 4 numbers probably with zeros also:");
+        for (int i = 0; i < aarr.Length; i++)
+        {
+            aarr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        //1 0 2 5
+        //0 1 2 3 -> i
+        //0 1 1 2 -> laastind++( after post increment)
+        for (int i = 0;i < aarr.Length; i++)
+        {
+            if (aarr[i] != 0)
+            {
+                int temp = aarr[laastind]; // 1 - 0 - 
+                aarr[laastind] = aarr[i]; // 1 - 2
+                aarr[i] = temp; // 1 - 0
+                laastind++; // 0 -> 1 ->2 
+
+            }
+        }
+        for (int i= 0;i < aarr.Length;i++)
+        {
+            Console.WriteLine(aarr[i]);
+        }
+    }
+}
