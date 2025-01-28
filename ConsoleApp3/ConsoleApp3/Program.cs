140b5f11b593822017e4dfe8f189e69c3915d977
HEMANTH R <120473382+LowHigh-Q@users.noreply.github.com>
2025-01-28 13:18:50 +0530

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+
+namespace Canada
+{
+    class Naanu
+    {
+        static void Main(string[] args)
+        {
+            int x = 4;
+            int[] arr = new int[x];
+            Console.WriteLine("Enter " + x + " numbers of elements");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int Evennumbs = 0;
+            int OddNumbs = 0;
+            foreach (int j in arr)
+            {
+                if (j % 2 == 0) { Evennumbs++; }
+                else { OddNumbs++; }
+            }
+
+            int[][] jarr = new int[2][];
+            jarr[0] = new int[Evennumbs];
+            jarr[1] = new int[OddNumbs];
+
+            int evenindex = 0;
+            int oddindex = 0;
+
+            foreach (int q in arr)
+            {
+                if (q % 2 == 0) { jarr[0][evenindex++] = q; }
+                else { jarr[1][oddindex++] = q; }
+            }
+            foreach (int d in jarr[0])
+            {
+                Console.WriteLine("Even Number: " + d);
+            }
+            foreach (int d in jarr[1])
+            {
+                Console.WriteLine("Odd number: " + d);
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/obj/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.obj
+{
+    public class Class1
+    {
+        static void zerotohero(string[] args)
+        {
+            Console.WriteLine("Enter the size the array: ");
+            int n = Convert.ToInt32(Console.Read());
+            int[] arr = new int[n];
+            int lastIndex = 0;
+            Console.WriteLine("Enter the array elements: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    int temp = arr[lastIndex];
+                    arr[lastIndex] = arr[i];
+                    arr[i] = temp;
+                    lastIndex++;
+                }
+
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
+       //int temp = arr[lastNonZeroFoundAt];
+        //arr[lastNonZeroFoundAt] = arr[i];
+        //        arr[i] = temp;
